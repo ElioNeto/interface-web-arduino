cc269df7ae722bf1626bb0ff888c6253ad9c067c
Elio Neto <netoo.elio@hotmail.com>
2019-05-26 22:40:38 -0300

    ta criando forma carai

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './Pages/App';
 import Create from './Pages/Create/Create';
 import './index.css';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
@@ -8,7 +7,7 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 ReactDOM.render(
     <BrowserRouter>
         <Switch>
-            <Route path="/" exact={true} component={App}/>
+
             <Route path="/Create" component={Create}/>
         </Switch>
     </ BrowserRouter>, 
