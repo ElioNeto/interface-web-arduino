ad7b6a32503fca90ac1ede11c3a025e8dce51f99
Elio Neto <netoo.elio@hotmail.com>
2019-05-27 19:39:19 -0300

    Pega esse Formulario porraaaaaaaaaa!!!!!

--- src/Pages/Create/Create.js
+++ src/Pages/Create/Create.js
@@ -11,6 +11,7 @@ import Col from 'react-bootstrap/Col';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
 import Button from 'react-bootstrap/Button';
+import Badge from 'react-bootstrap/Badge'
 
 import logo from './logo.svg';
 
@@ -18,16 +19,18 @@ class Create extends Component {
     render() {
       return (
         <div>
-        <Navbar bg="dark" variant="dark">
+        <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
           <Navbar.Brand href="#Create">
-          <h1><img 
+          <img 
             src={logo} 
-            width="50"
-            height="50"
+            width="30"
+            height="30"
             className="d-inline-block align-top"
             alt=""
-          /> &nbsp; Automação</h1>
+          /> Automação <Badge variant="secondary">New</Badge>
           </Navbar.Brand>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
             <Nav className="mr-auto">
             <Nav.Link href="#home">Home</Nav.Link>
             <Nav.Link href="#features">Features</Nav.Link>
@@ -36,26 +39,65 @@ class Create extends Component {
           <Form inline>
             <Button variant="outline-info">Logout</Button>
           </Form>
+          </Navbar.Collapse>
         </Navbar>
         <Container>
+          <row>&nbsp;</row>
+          <center>
+          <h2>
+            Cadastro Do Controle <Badge pill variant="danger">Versão 2.0.1</Badge>
+          </h2>
+          </center>
           <Form>
-            <Form.Group as={Row} controlId="formPlaintextEmail">
-              <Form.Label column sm="2">
-                Email
-              </Form.Label>
-              <Col sm="10">
-                <Form.Control plaintext readOnly defaultValue="email@example.com" />
-              </Col>
+            <Form.Row>
+              <Form.Group as={Col} controlId="formGridEmail">
+                <Form.Label>Email</Form.Label>
+                <Form.Control type="email" placeholder="Enter email" />
+              </Form.Group>
+
+              <Form.Group as={Col} controlId="formGridPassword">
+                <Form.Label>Password</Form.Label>
+                <Form.Control type="password" placeholder="Password" />
+              </Form.Group>
+            </Form.Row>
+
+            <Form.Group controlId="formGridAddress1">
+              <Form.Label>Address</Form.Label>
+              <Form.Control placeholder="1234 Main St" />
             </Form.Group>
 
-            <Form.Group as={Row} controlId="formPlaintextPassword">
-              <Form.Label column sm="2">
-                Password
-              </Form.Label>
-              <Col sm="10">
-                <Form.Control type="password" placeholder="Password" />
-              </Col>
+            <Form.Group controlId="formGridAddress2">
+              <Form.Label>Address 2</Form.Label>
+              <Form.Control placeholder="Apartment, studio, or floor" />
             </Form.Group>
+
+            <Form.Row>
+              <Form.Group as={Col} controlId="formGridCity">
+                <Form.Label>City</Form.Label>
+                <Form.Control />
+              </Form.Group>
+
+              <Form.Group as={Col} controlId="formGridState">
+                <Form.Label>State</Form.Label>
+                <Form.Control as="select">
+                  <option>Choose...</option>
+                  <option>...</option>
+                </Form.Control>
+              </Form.Group>
+
+              <Form.Group as={Col} controlId="formGridZip">
+                <Form.Label>Zip</Form.Label>
+                <Form.Control />
+              </Form.Group>
+            </Form.Row>
+
+            <Form.Group id="formGridCheckbox">
+              <Form.Check type="checkbox" label="Check me out" />
+            </Form.Group>
+
+            <Button variant="primary" type="submit">
+              Submit
+            </Button>
           </Form>
         </Container>   
         </div>          
